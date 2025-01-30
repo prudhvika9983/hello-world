ec8ac433564644545ad564badae5b78d6155684a
prudhvika9983 <prudhvikareddy9959@gmail.com>
2025-01-30 18:31:58 +0530

    Add files via upload

---
+++ Mutliplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Mutliplication {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int Multiply=a*b;
+        System.out.println("the mutliply of a and b" +  Multiply);
+        scanner.close();
+        
+    }
+    
+}
